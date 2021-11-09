9f2dbce0fd0910d306a4adf32dc4a1c2562d5d23
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2021-11-09 13:20:07 +0300

    Добавьте файлы проекта.

---
+++ Tsk14/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsk14
+{
+    abstract class Animal
+    {
+        public string Name;
+        public string Type;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Я {0}! {1}", Type, Name);
+        }
+    }
+    class PetShop
+    {
+        List<Animal> pets = new List<Animal>();
+
+        public void AddPet(Animal animal)
+        {
+            pets.Add(animal);
+        }
+
+        public void Say()
+        {
+            foreach (var p in pets)
+                p.ShowInfo();
+        }
+    }
+    class Dog : Animal
+    {
+        public Dog(string name,string type) 
+            : base()
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+    class Cat : Animal
+    {
+        public Cat(string name, string type) 
+            : base()
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PetShop petshop = new PetShop();
+            petshop.AddPet(new Dog("Гав!","Собака"));
+            petshop.AddPet(new Cat("Мяу!","Кот"));
+            petshop.Say();
+            Console.ReadLine();
+        }
+    }
+}
